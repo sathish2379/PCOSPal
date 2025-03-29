b05b2f116c2461f85d4965c0d1c2865e2673ea35
sathish2379 <sathishreddygathpa@gmail.com>
2025-03-29 16:27:10 -0700

    updated app.run

--- backend/app.py
+++ backend/app.py
@@ -306,4 +306,6 @@ def test_firebase():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
